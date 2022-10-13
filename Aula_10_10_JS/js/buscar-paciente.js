6e6d6f0653f783708895e6196d33502a71d4e9ad
Daniel Mescolin <72096313+Mescolin@users.noreply.github.com>
2022-10-13 10:39:33 -0300

    Add files via upload
    Aula do dia 10 de Outubro.

---
+++ Aula_10_10_JS/js/buscar-paciente.js
@@ -0,0 +1,14 @@
+var botaoBuscar=document.querySelector("#buscar-paciente");
+
+botaoBuscar.addEventListener("click",function(){
+    console.log("Buscando pacinetes...")
+    var xhr=new XMLHttpRequest;
+
+    xhr.open("GET","https://api-pacientes.herokuapp.com/pacientes");
+    
+    xhr.addEventListener("load",function(){
+        console.log(xhr.responseText)
+    });
+
+    xhr.send();
+});
